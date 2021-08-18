2cde2aa39e7b33fb8b83eb004550f42ce9a8a2f0
Piotr Kalota <piotrkalota99@gmail.com>
2021-08-18 20:51:09 +0200

    Project configuration

---
+++ src/main/java/com/cheaclo/clothesdatabase/ClothesDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.cheaclo.clothesdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheaclo/clothesdatabase/ClothesDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheaclo.clothesdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
