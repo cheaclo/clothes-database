19b6080e17f012605d149830f13235134b999040
Piotr Kalota <piotrkalota99@gmail.com>
2021-08-23 22:11:18 +0200

    Rename ModelProduct field

--- src/main/java/com/cheaclo/clothesdatabase/model/ModelProduct.java
+++ src/main/java/com/cheaclo/clothesdatabase/model/ModelProduct.java
@@ -5,11 +5,11 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class Product {
+public class ModelProduct {
     private String title;
     private String price;
     private String regularPrice;
-    private String shopUrl;
+    private String productUrl;
     private String imageUrl;
     private List<String> categories;
     private String type;
