657317966b0a9c332174a190904b10251256cc0e
Piotr Kalota <piotrkalota99@gmail.com>
2021-08-23 23:37:25 +0200

    Add initial insertAndUpdate implementation

--- src/main/java/com/cheaclo/clothesdatabase/service/DatabaseUpdater.java
+++ src/main/java/com/cheaclo/clothesdatabase/service/DatabaseUpdater.java
@@ -5,6 +5,7 @@ import com.cheaclo.clothesdatabase.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -13,7 +14,21 @@ public class DatabaseUpdater {
     private ProductRepository productRepository;
 
     public void insertAndUpdateProducts(List<Product> products) {
-        for (Product product : products)
-            productRepository.save(product);
+        for (Product product : products) {
+            Product dbProduct = productRepository.findFirstByHash(product.getHash());
+            if (dbProduct == null)
+                insertProduct(product);
+            else
+                updateProduct(dbProduct, product);
+        }
+    }
+
+    public void updateProduct(Product dbProduct, Product product) {
+        dbProduct.setLastUpdate(new Date());
+        productRepository.save(dbProduct);
+    }
+
+    public void insertProduct(Product product) {
+        productRepository.save(product);
     }
 }
