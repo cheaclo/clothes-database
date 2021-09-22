ead836ed93d18d509a2444556c801c073eea5c6b
Piotr Kalota <piotrkalota99@gmail.com>
2021-09-22 11:09:45 +0200

    Change save request mapping

--- src/main/java/com/cheaclo/clothesdatabase/controller/SaveProductController.java
+++ src/main/java/com/cheaclo/clothesdatabase/controller/SaveProductController.java
@@ -12,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/product")
 public class SaveProductController {
     @Autowired
     private SaveProductControllerResponse saveProductControllerResponse;
