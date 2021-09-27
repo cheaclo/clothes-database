a0c6c7cb679fd649f2574ec5201236df786390d4
Piotr Kalota <piotrkalota99@gmail.com>
2021-09-27 18:24:50 +0200

    Add ProductCategoryRepositoryTest

---
+++ src/test/java/com/cheaclo/clothesdatabase/repository/ProductCategoryRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.cheaclo.clothesdatabase.repository;
+
+import com.cheaclo.clothesdatabase.entity.ProductCategory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ProductCategoryRepositoryTest {
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+
+    private final String categoryName = "Underwear";
+    private final String nonExistingCategoryName = "Non existing category";
+
+    @Test
+    public void findFirstByNameIgnoreCaseTest() {
+        ProductCategory productCategory = productCategoryRepository.findFirstByNameIgnoreCase(categoryName);
+        assert(productCategory != null);
+
+        productCategory = productCategoryRepository.findFirstByNameIgnoreCase(categoryName.toUpperCase());
+        assert(productCategory != null);
+
+        ProductCategory nonExistingProductCategory = productCategoryRepository.findFirstByNameIgnoreCase(nonExistingCategoryName);
+        assert(nonExistingProductCategory == null);
+    }
+}
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO shop (shop_id, name) VALUES(1, 'MANGO')
 INSERT INTO sender (sender_id, name, authentication_code) VALUES(1, 'first.sender.name', 'codecodecode')
 INSERT INTO product_type (product_type_id, name) VALUES(1, 'UNISEX')
+INSERT INTO product_category (id, name) VALUES(1, 'Underwear')
